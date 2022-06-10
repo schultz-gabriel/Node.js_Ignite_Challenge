17d161053adb5c432036d275b5b3eae97edbe8d3
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2022-06-10 12:44:48 -0300

    adding /post to add new user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+function checksExistsUsername(request, response, next){
+  const {name, username} = request.body
+  const isUsernameAlreadyUsed = users.find((user) => user.username = username)
+  if(isUsernameAlreadyUsed){
+    return response.status(400).json({error: "Username is already used"})
+  }
+  request.user = {
+    name,
+    username,
+  }
+  return next()
+}
+
+app.post('/users', checksExistsUsername, (request, response) => {
+  const { user } = request
+  const newUser = {
+    id: uuidv4(),
+    name: user.name,
+    username: user.username,
+    todos: []
+  }
+
+  users.push(newUser)
+  
+  response.send(newUser)
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
